1f886e7d72bd91e9c3c4dd326bfd45fee43ac39b
felicia <faubert@csu.fullerton.edu>
2019-08-17 15:43:16 -0700

    fix3

--- scripts/nav.js
+++ scripts/nav.js
@@ -6,8 +6,8 @@ window.addEventListener('scroll', function() {
     document.getElementById('right').style.justifyContent = "flex-end";
     document.getElementById('right').style.fontSize = "16px";
     document.getElementById('list').style.width = "30%";
-    document.getElementById('left').style.margin = "0px -20px 0px -25px";
     document.getElementById('left').style.width = "20%";
+    document.getElementById('left').style.justifyContent = "flex-start";
     document.getElementById('left').style.fontSize = "26px";
     document.getElementById('border').style.height = "3.5rem";
 
@@ -18,7 +18,6 @@ window.addEventListener('scroll', function() {
     document.getElementById('right').style.justifyContent = "center";
     document.getElementById('right').style.fontSize = "22px";
     document.getElementById('list').style.width = "50%";
-    document.getElementById('left').style.margin = "0px 0px 0px 0px";
     document.getElementById('left').style.width = "100%";
     document.getElementById('left').style.fontSize = "32px";
     document.getElementById('border').style.height = "7.5rem";
