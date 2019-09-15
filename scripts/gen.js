30cacbc8db8b0a13cfacea144bc57cba8dbf0259
felicia <faubert@csu.fullerton.edu>
2019-09-14 17:26:27 -0700

    update

---
+++ scripts/gen.js
@@ -0,0 +1,26 @@
+
+
+var function = initialize_poputlation() {
+
+};
+
+var function = evaluate_fitness(){
+
+};
+
+var function = get_best_match(){
+
+};
+
+var function = crossover(){
+
+};
+
+var function = mutation(){
+
+};
+
+
+var function = geneticalgorithm(){
+
+}
